b3e12f1d33e98083fb270b199579429fa726fadb
Evans <ec.softwareengineer@gmail.com>
2023-02-16 09:57:24 -0800

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * Return 1
  */
 
-int main(void) {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, 42);
+int main(void) 
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }
