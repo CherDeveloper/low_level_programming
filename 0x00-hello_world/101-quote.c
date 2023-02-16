367891137b45f98ce341958dcdac1bd5e332ace0
Evans <ec.softwareengineer@gmail.com>
2023-02-16 07:43:47 -0800

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 # include <unistd.h>
+
 /**
  * main - fitfth entry
  * Return: 1
  */
 int main(void)
 {
-	const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - "
+             "Dora Korpar, 2015-10-19\n";
 	write(2, str, sizeof(str) - 1);
 	return (1);
 }
