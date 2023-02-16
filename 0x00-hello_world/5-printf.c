7e39e02d7a46d429d519cb0128d45fccb7d06ec8
Evans <ec.softwareengineer@gmail.com>
2023-02-16 06:34:00 -0800

    corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
 /**
  * main- second entry
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
