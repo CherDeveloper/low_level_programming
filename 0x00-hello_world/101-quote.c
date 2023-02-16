2bc8fb6c0d9a24b398fcec8c64ecd6a3b2c1c368
Evans <ec.softwareengineer@gmail.com>
2023-02-16 07:23:36 -0800

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	const char [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, str, sizeof(str) - 1);
 	return (1);
 }
