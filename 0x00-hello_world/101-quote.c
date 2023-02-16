82521422189dff993cb2dd2973bea7d8002ab83e
Evans <ec.softwareengineer@gmail.com>
2023-02-16 07:21:25 -0800

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - fitfth entry
+ * Return: 1
+ */
+int main(void)
+{
+	const char [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, sizeof(str) - 1);
+	return (1);
+}
