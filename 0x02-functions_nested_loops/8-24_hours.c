a5293538225d649782fbbc7de7c54bbe68d0e261
Evans <ec.softwareengineer@gmail.com>
2023-02-21 11:06:42 -0800

    corrected

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,8 @@
 
 
 /**
- * jack_bauer - prints every minute of the day
+ * jack_bauer - prints every minute of the day,
+ * starting from 00:00 to 23:59
  * Return: - always 0 (success)
  */void jack_bauer(void)
 {
