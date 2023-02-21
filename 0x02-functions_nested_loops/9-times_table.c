52498d3f29f45162c709b45877cce3be6ed18bcf
Evans <ec.softwareengineer@gmail.com>
2023-02-21 12:06:09 -0800

    print 9 times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,22 +10,22 @@ void times_table(void)
 	int b;
 	int c;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 9; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b < 9; b++)
 		{
 			c = b * a;
 			if (b == 0)
 			{
 				_putchar(c + '0');
 			}
-			if (c < 10 && b != 0)
+			if (c < 9 && b != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0');
-			} else if (c >= 10)
+			} else if (c >= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
