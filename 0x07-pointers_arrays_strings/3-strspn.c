4da9d0731c8a0c14f1c533e989a48162a6ed3d06
Evans <ec.softwareengineer@gmail.com>
2023-03-06 12:57:54 -0800

    corrected

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, len = 0, match = 0;
 
-	for (i = 0; s[i] !='\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
