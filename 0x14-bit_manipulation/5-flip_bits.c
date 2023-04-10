46a4594db44e850870662547438c523079abbe11
Evans <ec.softwareengineer@gmail.com>
2023-04-10 12:54:37 -0700

    returns the number of bits you would need to flip to get from one number to anothe

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * flip_bits - counts the number of bits needed.
+ * @n: The number.
+ * Return: The number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int che = n ^ m, bits = 0;
+
+	while (che > 0)
+	{
+		bits += (che & 1);
+		che >>= 1;
+	}
+
+	return (bits);
+}
